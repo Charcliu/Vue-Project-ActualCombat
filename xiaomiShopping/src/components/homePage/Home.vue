38c03dc05039d2e18b881bf108877c32c0ef560b
Fluently <f_liuchang@163.com>
2018-05-05 18:31:18 +0800

    Home page header

--- xiaomiShopping/src/components/homePage/Home.vue
+++ xiaomiShopping/src/components/homePage/Home.vue
@@ -1,21 +1,24 @@
 <template>
     <div class = "home">
-        <h1>Home</h1>
+        <Header></Header>
     </div>
 </template>
 
 <script>
+    import Header from './header/Header'
     export default {
         name: 'Home',
         data () {
             return {
                 
             }
+        },
+        components : {
+            Header
         }
     }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    
 </style>
---
+++ xiaomiShopping/src/components/homePage/header/Header.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class = "header">
+        <div>
+            <span v-for = "(item,index) in headerMsg" class = "headerSpan">
+                {{item}}
+                <span v-if = "index != headerMsg.length - 1">|</span>
+            </span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Header',
+        data () {
+            return {
+                headerMsg : ["小米商城","MIUI","IoT","云服务","水滴","金融","有品","Select Region"]
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .header{
+        height: 40px;
+        font-size: 12px;
+        color: #b0b0b0;
+        background: #333;
+        line-height: 40px;
+    }
+    .header > div{
+        width: 1226px;
+        margin: auto;
+    }
+    .headerSpan span{
+        color: #424242;
+        margin: 0 .5em;
+    }
+    .headerSpan:hover{
+        color: white;
+    }
+    .headerSpan{
+        cursor: pointer;
+    }
+</style>
