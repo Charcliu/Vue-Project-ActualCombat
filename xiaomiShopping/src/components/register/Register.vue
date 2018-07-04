a1d6288dd46546f013d80a3f5f8c94aed9cf179e
Wofluently <f_liuchang@163.com>
2018-07-04 22:34:07 +0800

    Add Sass feature to My Vue Project

--- xiaomiShopping/src/components/register/Register.vue
+++ xiaomiShopping/src/components/register/Register.vue
@@ -1,224 +1,235 @@
 <template>
-    <div class = "register">
-        <div class = "layout">
-            <div>
-                <a class = "milogo"></a>
-            </div>
-            <div>
-                <span class = "accoutSpan">注册小米账号</span>
-            </div>
-            <div class = "registerInfo">
-                <div>
-                    <span>用户名：</span>
-                    <input type = "text" placeholder = "请输入用户名" v-model = "userName"/>
-                </div>
-                <div>
-                    <span>密码：</span>
-                    <input type = "password" placeholder = "请输入密码" v-model = "passWord"/>
-                </div>
-            </div>
-            <div>
-                <div class= "registerButton" @click = "registerUser">
-                    <span>注册</span>
-                </div>
-            </div>
-            <div class = "login">
-                <span @click = "goLogin">立即登录</span>
-            </div>
-        </div>
-        <Notice :notice-info = "noticeInfo"></Notice>
+<div class="register">
+  <div class="layout">
+    <div>
+      <a class="milogo"></a>
     </div>
+    <div>
+      <span class="accoutSpan">注册小米账号</span>
+    </div>
+    <div class="registerInfo">
+      <div>
+        <span>用户名：</span>
+        <input type="text" placeholder="请输入用户名" v-model="userName" />
+      </div>
+      <div>
+        <span>密码：</span>
+        <input type="password" placeholder="请输入密码" v-model="passWord" />
+      </div>
+    </div>
+    <div>
+      <div class="registerButton" @click="registerUser">
+        <span>注册</span>
+      </div>
+    </div>
+    <div class="login">
+      <span @click="goLogin">立即登录</span>
+    </div>
+  </div>
+  <Notice :notice-info="noticeInfo"></Notice>
+</div>
 </template>
 
 <script>
-
-import { mapMutations, mapGetters } from 'vuex'
+import {
+  mapMutations,
+  mapGetters
+} from 'vuex'
 // 公共弹框组件
 import Notice from '../base/Notice'
 
 export default {
-    name: 'Register',
-    data () {
-        return {
-            userName : "",
-            passWord : "",
-            noticeInfo : {
-                show : false,
-                title : "",
-                content : "",
-                ok : {
-                    show : false,
-                    callBack : null
-                },
-                cancel : {
-                    show : false,
-                    callBack : null
-                }
-            },
-            radio : '1'
-        }
-    },
-    computed : {
-        ...mapGetters([
-            'gettersUserInfo'
-        ]),
-        getErrorMsg : function(){
-            let errorType = {
-                status : false,
-                title : "",
-                type : ""
-            };
-
-            if(this.userName != ""){
-                let status = this.gettersUserInfo.some(function(item){
-                    return item.userName == this.userName;
-                }, this);
-
-                if(status){
-                    errorType = {
-                        status : true,
-                        title : "Tips",
-                        type : "UserName Can not Repeat."
-                    }
-                }
-            }else{
-                errorType = {
-                    status : true,
-                    title : "Tips",
-                    type : "UserName Can not Be Empty."
-                }
-            }
-            return errorType;
-        }
-    },
-    methods : {
-        ...mapMutations([
-            'addUser'
-        ]),
-        registerUser : function(){
-            let errorType = this.getErrorMsg;
-
-            if(!errorType.status){
-                this.addUser({
-                    userName : this.userName,
-                    passWord : this.passWord
-                })
-                // elementUI 提示组件
-                this.$message({
-                    message: '恭喜你，注册成功！',
-                    type: 'success'
-                });
-            }else{
-                let _this = this;
-                this.noticeInfo = {
-                    show : errorType.status,
-                    title : errorType.title,
-                    content : errorType.type,
-                    ok : {
-                        show : true,
-                        callBack : function(){
-                            _this.noticeInfo.show = false;
-                        }
-                    },
-                    cancel : {
-                        show : false,
-                        callBack : null
-                    }
-                }
-            }
+  name: 'Register',
+  data() {
+    return {
+      userName: "",
+      passWord: "",
+      noticeInfo: {
+        show: false,
+        title: "",
+        content: "",
+        ok: {
+          show: false,
+          callBack: null
         },
-        goLogin : function(){
-            this.$router.push('login');
+        cancel: {
+          show: false,
+          callBack: null
         }
-    },
-    components : {
-        Notice
+      },
+      radio: '1'
     }
+  },
+  computed: {
+    ...mapGetters([
+      'gettersUserInfo'
+    ]),
+    getErrorMsg: function() {
+      let errorType = {
+        status: false,
+        title: "",
+        type: ""
+      };
+
+      if (this.userName != "") {
+        let status = this.gettersUserInfo.some(function(item) {
+          return item.userName == this.userName;
+        }, this);
+
+        if (status) {
+          errorType = {
+            status: true,
+            title: "Tips",
+            type: "UserName Can not Repeat."
+          }
+        }
+      } else {
+        errorType = {
+          status: true,
+          title: "Tips",
+          type: "UserName Can not Be Empty."
+        }
+      }
+      return errorType;
+    }
+  },
+  methods: {
+    ...mapMutations([
+      'addUser'
+    ]),
+    registerUser: function() {
+      let errorType = this.getErrorMsg;
+
+      if (!errorType.status) {
+        this.addUser({
+          userName: this.userName,
+          passWord: this.passWord
+        })
+        // elementUI 提示组件
+        this.$message({
+          message: '恭喜你，注册成功！',
+          type: 'success'
+        });
+      } else {
+        let _this = this;
+        this.noticeInfo = {
+          show: errorType.status,
+          title: errorType.title,
+          content: errorType.type,
+          ok: {
+            show: true,
+            callBack: function() {
+              _this.noticeInfo.show = false;
+            }
+          },
+          cancel: {
+            show: false,
+            callBack: null
+          }
+        }
+      }
+    },
+    goLogin: function() {
+      this.$router.push('login');
+    }
+  },
+  components: {
+    Notice
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang = "scss" scoped>
-    .register{
-        position: fixed;
-        left: 0px;
-        top: 0px;
-        height: 100%;
-        width: 100%;
-    }
-    .layout{
-        width: 854px;
-        margin: 0 auto;
-    }
-    .layout > div{
-        width: 100%;
-        position: relative;
-        margin-top: 50px;
-    }
-    .milogo{
-        background-image: url(../../assets/milogo.png);
-        text-decoration: none;
-        width: 49px;
-        height: 48px;
-        margin: 0 auto;
-        display: block;
-        cursor: default;
-    }
-    .accoutSpan{
-        font-size: 28px;
-        font-family: arial,"Hiragino Sans GB", "Microsoft YaHei","微軟正黑體","儷黑 Pro", sans-serif;
-        display: block;
-        margin: auto;
-        width: 200px;
-        text-align: center;
-    }
-    .registerInfo > div{
-        width: 300px;
-        margin: auto;
-        font-size: 20px;
-        margin-bottom: 10px;
-    }
-    .registerInfo span{
-        display: inline-block;
-        width: 80px;
-    }
-    .registerInfo input{
-        width: 210px;
-        height: 25px;
-    }
+/* import 通用SASS 可以省略.sass .scss后缀 _开头的sass文件为局部文件，不会编译成css 调用时可省略_*/
+/* @import "../../themes/common";
+default作用于变量，表示如果导入的sass文件中和你存在相同的变量，就用默认采用sass中声明的变量。
+$bgColor: green !default;
+div{
+  background-color: $bgColor;
+} */
 
-    /* Test Sass */
-    $register_button_color: #ff6700;
 
-    .registerButton{
-        width: 300px;
-        margin: auto;
-        text-align: center;
-        background-color: $register_button_color;
-        border: 1px solid rgba(186,186,186,0.3);
-        color: #fff;
-        height: 42px;
-        line-height: 42px;
-        cursor: pointer;
-    }
-    .errorTips{
-        width: 300px;
-        margin: auto;
-        text-align: center;
-        color: red;
-        font-size: 15px;
-    }
-    .login{
-        margin: auto;
-        text-align: right;
-        color: #757575;
-        width: 304px !important;
-        margin-top: 5px !important;
-    }
-    .login > span{
-        cursor: pointer;
-    }
-    .login > span:hover{
-        color: #ef5b00;
-    }
+.register {
+    position: fixed;
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: 100%;
+}
+.layout {
+    width: 854px;
+    margin: 0 auto;
+}
+.layout > div {
+    width: 100%;
+    position: relative;
+    margin-top: 50px;
+}
+.milogo {
+    background-image: url("../../assets/milogo.png");
+    text-decoration: none;
+    width: 49px;
+    height: 48px;
+    margin: 0 auto;
+    display: block;
+    cursor: default;
+}
+.accoutSpan {
+    font-size: 28px;
+    font-family: arial,"Hiragino Sans GB", "Microsoft YaHei","微軟正黑體","儷黑 Pro", sans-serif;
+    display: block;
+    margin: auto;
+    width: 200px;
+    text-align: center;
+}
+.registerInfo > div {
+    width: 300px;
+    margin: auto;
+    font-size: 20px;
+    margin-bottom: 10px;
+}
+.registerInfo span {
+    display: inline-block;
+    width: 80px;
+}
+.registerInfo input {
+    width: 210px;
+    height: 25px;
+}
+
+/* Test Sass */
+$register_button_color: #ff6700;
+
+.registerButton {
+    width: 300px;
+    margin: auto;
+    text-align: center;
+    background-color: $register_button_color;
+    border: 1px solid rgba(186,186,186,0.3);
+    color: #fff;
+    height: 42px;
+    line-height: 42px;
+    cursor: pointer;
+}
+.errorTips {
+    width: 300px;
+    margin: auto;
+    text-align: center;
+    color: red;
+    font-size: 15px;
+}
+.login {
+    margin: auto;
+    text-align: right;
+    color: #757575;
+    width: 304px !important;
+    margin-top: 5px !important;
+}
+.login > span {
+    cursor: pointer;
+}
+.login > span:hover {
+    color: #ef5b00;
+}
 </style>
